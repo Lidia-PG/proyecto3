116445131791c7afb3ec84560cb2c2e48944a27d
lidia pérez <ufonicap@gmail.com>
2024-01-29 12:37:46 +0100

    new dev branch and import UserList

---
+++ src/components/UserList.jsx
@@ -0,0 +1,15 @@
+function UserList(){
+
+const [user, setUser] = useState('');
+
+    return(
+        <>
+       <h1>Lista de alumnos</h1>
+        <label htmlfor=""></label>
+        <input type="text"/>
+        <button></button>
+        
+        </>
+    )
+}
+export default UserList
