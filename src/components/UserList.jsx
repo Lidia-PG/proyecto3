77e8ec9dde907cee449aa9fabcf66c7f89ac02be
unknown <ufonicap@gmail.com>
2024-01-31 09:29:14 +0100

    update

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,52 +1,86 @@
 import { useState, useEffect } from "react";
+import { UserService } from "../userService";
 
-function UserList() {
-    const [user, setUser] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [occasion, setOccasion] = useState('');
+const UserList = () =>{
+
+    const [user, setUser] = useState(
+        //así queda un registro de usuario que consta de dos datos string en lugar de uno solo. Antes teníamos un strint, ahora
+        //tenemos un objeto con dos propiedades, que son un string cada uno. Lo inicializamos vacío porque los datos vienen 
+        //desde el frontend. Obvioooooo
+        {
+            userName: "",
+            userSurname: ""
+        }
+        );
+
+    //Antes enviabamos aquí los usuarios desde el botón del formulario. Ahora no. Ahora esos usuarios se almacenan directamente
+    //en nuestro archivo json. Ahora esta variable se usa para cargar esos datos almacenados en el json
     const [userList, setUserList] = useState([]);
 
-    function handleNameChange(e){
-        setUser(e.target.value);
-    };
+    //esta es la función que carga los datos almacenados en el json
+    async function getData(){
+        //accedo a UserService, en concreto a su métod GET. Como este método al ejecutarse retorna los datos de los usuarios,
+        //lo que almaceno en la variable users son esos datos.
+        let users = await UserService.getAllUsers();
 
-    function handleLastNameChange(e){
-        setLastName(e.target.value);
-    };
+        //ahora actualizo el estado de userList con esta variable (usuarios)
+        setUserList(users)  
 
-    function handleOccasionChange(e){
-        setOccasion(e.target.value);
-    };
 
-    function handleAddUserToList() {
-        setUserList(prevUserList => [...prevUserList, user + ' ' + lastName + ' ' + occasion]);
-        setUser('');
-        setLastName('');
-        setOccasion('');  
     }
 
+    //LLamo a getData() Si no los datos no vienen nunca!!!!
+
+    getData();
+
+    function handleNameChange(e){
+
+        
+        setUser({...user, [e.target.name]:e.target.value})
+    };
+
+
+
+    async function handleAddUserToList() {
+
+        //aquí hay que sustuir este envío de usuarios al array userList por el envío al archivo json accediendo a un método
+        //POST que definamos en userService
+        await UserService.submitUser(user);
+    
+
+     //el reset del formulario y por tanto de la variable user una vez enviados los datos
+        setUser({
+            userName: "",
+            userSurname: ""
+        });
+
+      }
+
     return(
+
         <>
-        <h1>Lista de alumnos</h1>
+        <h1>Qué lista eres</h1>
 
-        <label htmlFor="userName">Nombre:</label>
-        <input type="text" name="userName" value={user} onChange={handleNameChange}/>
+{/* hemos añadido  otro input y otro label. Al escribir, cada uno actualiza una propiedad del objeto user*/}
+        <label htmlFor="userName"></label>
+        <input type="text" name="userName" value={user.userName} onChange={handleNameChange}/>
 
-        <label htmlFor="userLastName">Apellido:</label>
-        <input type="text" name="userLastName" value={lastName} onChange={handleLastNameChange}/>
-
-        <label htmlFor="userOccasion">Evento:</label>
-        <input type="text" name="userOccasion" value={occasion} onChange={handleOccasionChange}/>
+        <label htmlFor="userSurname"></label>
+        <input type="text" name="userSurname" value={user.userSurname} onChange={handleNameChange}/>
 
         <button onClick={handleAddUserToList}>Añadir usuario</button>
-        <ol>
+        <ul>
+
+{/* antes, como userList era un array de strings, después del map solo teníamos que imprimir la variable "user",
+ahora user es un objeto, para imprimir la información accedo a cada una de las propiedades por separado (dentro del map) */}
         {
             userList.map((user, index)=>(
-                <li key={index}> {user} </li>
+                <li key={index}> {user.userName} {user.userSurname} </li>
             ))
         }
-        </ol>
+        </ul>
         </>
+        
     )
 }
 
