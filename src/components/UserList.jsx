f1ae532132b3db66dc92556ecf6bd8350c54dac0
lidia pérez <ufonicap@gmail.com>
2024-01-31 16:28:02 +0100

    changes

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,35 +1,29 @@
-npimport { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { UserService } from "../userService";
 
 const UserList = () =>{
 
     const [user, setUser] = useState(
-        //así queda un registro de usuario que consta de dos datos string en lugar de uno solo. Antes teníamos un strint, ahora
-        //tenemos un objeto con dos propiedades, que son un string cada uno. Lo inicializamos vacío porque los datos vienen 
-        //desde el frontend. Obvioooooo
+    
         {
             userName: "",
             userSurname: ""
         }
         );
 
-    //Antes enviabamos aquí los usuarios desde el botón del formulario. Ahora no. Ahora esos usuarios se almacenan directamente
-    //en nuestro archivo json. Ahora esta variable se usa para cargar esos datos almacenados en el json
     const [userList, setUserList] = useState([]);
 
-    //esta es la función que carga los datos almacenados en el json
+    
     async function getData(){
-        //accedo a UserService, en concreto a su métod GET. Como este método al ejecutarse retorna los datos de los usuarios,
-        //lo que almaceno en la variable users son esos datos.
+        
         let users = await UserService.getAllUsers();
 
-        //ahora actualizo el estado de userList con esta variable (usuarios)
+        
         setUserList(users)  
 
 
     }
 
-    //LLamo a getData() Si no los datos no vienen nunca!!!!
 
     getData();
 
@@ -43,12 +37,11 @@ const UserList = () =>{
 
     async function handleAddUserToList() {
 
-        //aquí hay que sustuir este envío de usuarios al array userList por el envío al archivo json accediendo a un método
-        //POST que definamos en userService
+      
         await UserService.submitUser(user);
     
 
-     //el reset del formulario y por tanto de la variable user una vez enviados los datos
+    
         setUser({
             userName: "",
             userSurname: ""
